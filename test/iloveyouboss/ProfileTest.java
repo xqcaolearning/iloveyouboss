b0c4ea30e2447dc722739132081e546084f28cd8
guan yuan <yuan.guan89@gmail.com>
2016-01-20 18:47:36 +0800

    Add one case for include a must match criterion. Guan Yuan & Xiaoqi

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -13,6 +13,14 @@ public class ProfileTest {
     private Criterion criterion;
     private Answer answerRight, answerWrong;
     private Question question;
+    private Answer mustMatchRightAnswer;
+    private Answer mustMatchWrongAnswer;
+    private Answer matchRightAnswer;
+    private Answer matchWrongAnswer;
+    private Answer notMatchRightAnswer1;
+    private Answer notMatchWrongAnswer1;
+    private Answer notMatchRightAnswer2;
+    private Answer notMatchWrongAnswer2;
 
     @Before
     public void init() {
@@ -24,6 +32,21 @@ public class ProfileTest {
         criteria = new Criteria();
         criteria.add(criterion);
         profile = new Profile("profile");
+    }
+
+    @Before
+    public void init_common_variables() {
+        BooleanQuestion matchQuestion = new BooleanQuestion(1, "match question");
+        matchRightAnswer = new Answer(matchQuestion, 1);
+        matchWrongAnswer = new Answer(matchQuestion, 2);
+
+        BooleanQuestion notMatchQuestion1 = new BooleanQuestion(1, "not match question");
+        notMatchRightAnswer1 = new Answer(notMatchQuestion1, 1);
+        notMatchWrongAnswer1 = new Answer(notMatchQuestion1, 2);
+
+        BooleanQuestion notMatchQuestion2 = new BooleanQuestion(1, "not match question");
+        notMatchRightAnswer2 = new Answer(notMatchQuestion2, 1);
+        notMatchWrongAnswer2 = new Answer(notMatchQuestion2, 2);
     }
 
     @Test
@@ -51,6 +74,42 @@ public class ProfileTest {
         profile.add(answerRight);
         boolean result = profile.matches(criteria);
         assertThat(result, is(true));
+        assertThat(profile.score(), is(1000));
+    }
+
+    @Test
+    public void should_return_false_when_give_must_match_and_not_match_criteria() {
+        Criterion criterion2 = new Criterion(notMatchRightAnswer1, Weight.MustMatch);
+        Criterion criterion1 = new Criterion(notMatchRightAnswer2, Weight.Important);
+
+        Criteria criteria1 = new Criteria();
+        criteria1.add(criterion1);
+        criteria1.add(criterion2);
+
+        profile = new Profile("profile");
+        profile.add(notMatchWrongAnswer1);
+        profile.add(notMatchWrongAnswer2);
+
+        boolean result = profile.matches(criteria1);
+        assertThat(result, is(false));
+        assertThat(profile.score(), is(0));
+    }
+
+    @Test
+    public void should_return_false_when_give_must_match_and_one_match_criteria() {
+        Criterion mustMatchCriterion = new Criterion(notMatchRightAnswer1, Weight.MustMatch);
+        Criterion matchCriterion = new Criterion(matchRightAnswer, Weight.Important);
+
+        Criteria criteria1 = new Criteria();
+        criteria1.add(matchCriterion);
+        criteria1.add(mustMatchCriterion);
+
+        profile = new Profile("profile");
+        profile.add(notMatchWrongAnswer1);
+        profile.add(matchRightAnswer);
+
+        boolean result = profile.matches(criteria1);
+        assertThat(result, is(false));
         assertThat(profile.score(), is(1000));
     }
 }
