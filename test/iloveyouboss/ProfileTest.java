1bf8b2392a1a88dd353f4fed6a57e37c398b2885
guan yuan <yuan.guan89@gmail.com>
2016-01-20 13:20:42 +0800

    [Yuan]: add test case: should_return_true_when_give_all_answers_match_with_criteria

--- test/iloveyouboss/ProfileTest.java
+++ test/iloveyouboss/ProfileTest.java
@@ -9,16 +9,21 @@ import static org.junit.Assert.*;
 public class ProfileTest {
 
     private Profile profile;
-    private Criteria emptyCriteria;
+    private Criteria criteria, emptyCriteria;
+    private Criterion criterion;
+    private Answer answerRight, answerWrong;
+    private Question question;
 
     @Before
-    public void init_profile() {
-        profile = new Profile("profile");
-    }
-
-    @Before
-    public void init_empty_criteria() {
+    public void init() {
+        question = new BooleanQuestion(1, "question");
+        answerRight = new Answer(question, 1);
+        answerWrong = new Answer(question, 2);
+        criterion = new Criterion(answerRight, Weight.Important);
         emptyCriteria = new Criteria();
+        criteria = new Criteria();
+        criteria.add(criterion);
+        profile = new Profile("profile");
     }
 
     @Test
@@ -35,16 +40,17 @@ public class ProfileTest {
 
     @Test
     public void should_return_false_when_give_not_match_criteria() {
-        Question question = new BooleanQuestion(1, "question");
-        Answer oneAnswer = new Answer(question, 1);
-        Answer anotherAnswer = new Answer(question, 2);
-        Criterion criterion = new Criterion(oneAnswer, Weight.Important);
-        Criteria criteria = new Criteria();
-        criteria.add(criterion);
-        profile.add(anotherAnswer);
-
+        profile.add(answerWrong);
         boolean result = profile.matches(criteria);
         assertThat(result, is(false));
         assertThat(profile.score(), is(0));
+    }
+
+    @Test
+    public void should_return_true_when_give_all_answers_match_with_criteria() {
+        profile.add(answerRight);
+        boolean result = profile.matches(criteria);
+        assertThat(result, is(true));
+        assertThat(profile.score(), is(1000));
     }
 }
